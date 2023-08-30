0cc1715304582da58cbae9baf2b2efa4197510a3
Enrique Hervás Guerrero <enrique_hervas_94@hotmail.com>
2023-08-30 20:21:34 +0200

    Fixing variable names

--- twilio_config.py
+++ twilio_config.py
@@ -1,4 +1,4 @@
-TWILIO_ACCOUNT_SID ='XxYyZzXxYyZzXxYyZzXxYyZzXxYyZzXxYyZz'
-TWILIO_AUTH_TOKEN ='XxYyZzXxYyZzXxYyZzXxYyZzXxYyZzXxYyZz'
-PHONE_NUMBER ='phone number'
-API_KEY_WAPI = 'XxYyZzXxYyZzXxYyZzXxYyZzXxYyZzXxYyZz'
+twilio_account_sid = 'XxYyZzXxYyZzXxYyZzXxYyZzXxYyZzXxYyZz'
+twilio_auth_token = 'XxYyZzXxYyZzXxYyZzXxYyZzXxYyZzXxYyZz'
+phone_number = 'phone number'
+api_key_wapi = 'XxYyZzXxYyZzXxYyZzXxYyZzXxYyZzXxYyZz'
